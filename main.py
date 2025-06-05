92d5e0ea758b9a6c9023100fd8c80e29cf073442
devamsshah <dshah97@uic.edu>
2025-06-05 18:51:09 -0500

    main

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ import time
 from rl import TrafficEnv
 from stable_baselines3 import DQN
 
-ACTIONS = {
-    0: lambda: brake(),
-    1: lambda: left(),
-    2: lambda: right()
-}
-
-env = TrafficEnv(get_frame_fn=get_frame, action_fn=ACTIONS)
-model = DQN("CnnPolicy", env, verbose=1)
-model.learn(total_timesteps=10000)
-
-
-#get_frame()
-#focus_on_iphone()
-#enter_to_allow()
-#focus_on_iphone()
-#click_on_play()
-#get_frame()
-#get_frame()
-#get_frame()
+#ACTIONS = {
+#    0: lambda: brake(),
+#    1: lambda: left(),
+#    2: lambda: right()
+#}
+#
+#env = TrafficEnv(get_frame_fn=get_frame, action_fn=ACTIONS)
+#model = DQN("CnnPolicy", env, verbose=1)
+#model.learn(total_timesteps=10000)
+#
+#
+get_frame()
+focus_on_iphone()
+enter_to_allow()
+focus_on_iphone()
+click_on_play()
+get_frame()
+get_frame()
+get_frame()
